a3ef40993ac83e4f9bb737685a45ecfa92dab341
tamle6099 <tamle6099@gmail.com>
2024-12-04 10:15:45 +0700

    fix code

---
+++ .next/server/middleware-react-loadable-manifest.js
@@ -0,0 +1,1 @@
+self.__REACT_LOADABLE_MANIFEST="{\"app\\\\(dashboard)\\\\page.tsx -> react-apexcharts\":{\"id\":\"app\\\\(dashboard)\\\\page.tsx -> react-apexcharts\",\"files\":[\"static/chunks/_app-pages-browser_node_modules_react-apexcharts_dist_react-apexcharts_min_js.js\"]}}"

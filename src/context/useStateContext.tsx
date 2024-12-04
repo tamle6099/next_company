6446937cdde1b8cfeade266c685e4a1fb0e2bb8c
tamle6099 <tamle6099@gmail.com>
2024-12-04 10:06:31 +0700

    code new

--- src/context/useStateContext.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function useStateContext() {
-  return (
-    <div>useStateContext</div>
-  )
-}
-
-export default useStateContext
--- src/utils/AppConfig.ts
+++
@@ -1,9 +0,0 @@
-export const AppConfig = {
-  site_name: 'OCC Office',
-  title: 'OCC',
-  description: 'OCC Office',
-  url: '',
-  locale: 'en',
-  author: 'Anonymous',
-  pagination_size: 5,
-};
